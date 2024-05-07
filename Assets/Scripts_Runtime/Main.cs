d60148e583e66fe0c85a66f21a6ade29d7c7f368
Jackutea <chenwansal1@163.com>
2024-05-07 20:35:34 +0800

    ESTABLISH

---
+++ Assets/Scripts_Runtime/Main.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Shooting3DTutorial {
+
+    public class Main : MonoBehaviour {
+        void Start() {
+            Debug.Log("Hell");
+        }
+
+        void Update() {
+
+        }
+    }
+}
