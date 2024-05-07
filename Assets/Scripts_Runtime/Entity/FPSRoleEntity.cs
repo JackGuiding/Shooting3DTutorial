69e79faca6e01a4b7bee4e93dc25533f0f446b95
Jackutea <chenwansal1@163.com>
2024-05-07 22:00:54 +0800

    <doc> add: 文档说明

--- Assets/Scripts_Runtime/Entity/FPSRoleEntity.cs
+++ Assets/Scripts_Runtime/Entity/FPSRoleEntity.cs
@@ -10,6 +10,23 @@ namespace Shooting3DTutorial {
         void Awake() {
             // Cursor.visible = false;
             Cursor.lockState = CursorLockMode.Locked;
+
+            Rigidbody rb = GetComponent<Rigidbody>();
+            rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+
+            byte light = 0b00000000;
+            light |= 1 << 0;
+            light |= 1 << 4;
+
+            // 0b00010001 light
+            // 0b00000001 1灯
+            if ((light & (1 << 0)) != 0) {
+                Debug.Log("0");
+            }
+            if ((light & (1 << 1)) != 0) {
+                Debug.Log("1");
+            }
+
         }
 
         public void Tick(float dt) {
@@ -27,10 +44,14 @@ namespace Shooting3DTutorial {
 
                 Camera cam = Camera.main;
                 Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
-                int layerMask = 1 << 0;
+                int layerMask = 0b00000000_00000000_00000000_00000000;
+                // layerMask |= (1 << 0); // 0b00000000_00000000_00000000_00001000
+                layerMask |= (1 << 3); // 0b00000000_00000000_00000000_00001000
+                layerMask |= (1 << 7); // 0b00000000_00000000_00000000_10001000
                 bool hasHit = Physics.Raycast(ray, out RaycastHit hit, bulletFlyDistance, layerMask);
                 if (hasHit) {
                     GameObject target = hit.collider.gameObject;
+                    Debug.Log("Hit: " + target.name);
                     GameObject.Destroy(target);
                 }
             }
